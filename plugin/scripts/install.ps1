f3d1ec7f275079f44857d41d47ec599b28d4a649
Armin Zou <arminzou@outlook.com>
2026-08-26 23:02:04 -0400

    fix(release): pin first-run installers to 0.1.3
    - installers still fetched @v0.1.2 after the version bump

--- plugin/scripts/install.ps1
+++ plugin/scripts/install.ps1
@@ -3,7 +3,7 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$DotbrainVersion = "0.1.2"
+$DotbrainVersion = "0.1.3"
 $DotbrainRef = "git+https://github.com/arminzou/dotbrain@v$DotbrainVersion"
 
 function Write-Die($Message) {
