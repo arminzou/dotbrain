22f004e16c489bc4bd7a57ee5ddee05cc7941139
Armin Zou <arminzou@outlook.com>
2026-09-25 23:39:53 -0400

    chore(release): bump dotbrain to 0.4.1

--- plugin/scripts/install.sh
+++ plugin/scripts/install.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-readonly DOTBRAIN_VERSION="0.4.0"
+readonly DOTBRAIN_VERSION="0.4.1"
 readonly DOTBRAIN_REF="git+https://github.com/arminzou/dotbrain@v${DOTBRAIN_VERSION}"
 
 log() { printf '[dotbrain] %s\n' "$*"; }
